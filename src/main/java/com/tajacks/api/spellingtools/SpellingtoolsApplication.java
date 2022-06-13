3b0fdf6c26b76ae3f602b76c4b3785c6b5d7e775
tajacks <tom@tomjack.ca>
2022-06-12 21:42:15 -0400

    first commit

---
+++ src/main/java/com/tajacks/api/spellingtools/SpellingtoolsApplication.java
@@ -0,0 +1,13 @@
+package com.tajacks.api.spellingtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpellingtoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpellingtoolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tajacks/api/spellingtools/SpellingtoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tajacks.api.spellingtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpellingtoolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
